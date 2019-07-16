b1d22af7b258fbb712a4708bc21b822157a77b48
Joshua <joshuagomersallgebba@gmail.com>
2019-07-16 15:06:32 +0100

    Align Center -Added

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -6,8 +6,32 @@ insert into company (id,name) values (  1,'Valenciargo');
 insert into company (id,name) values (  2,'M4g1yK Tr1cKz');
 insert into company (id,name) values (  3,' ThisGuyOnceSaid');
 
-insert into computer (id,name,introduced,discontinued,company_id) values (573,'Gateway LT3103U','2008-01-01',null,null);
+insert into computer (id,name,introduced,discontinued,company_id) values (1,'Gateway LT3103U','2008-01-01',null,null);
+insert into computer (id,name,introduced,discontinued,company_id) values (2,'iPhone 4S','2011-10-14',null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (3,'iPh9one 4S','2011-10-14',null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (4,'iPho4ne 4S','2011-10-14',null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (5,'iPhonDWAe 4S','2011-10-14',null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (6,'iPhonAWDWAeD 4S','2011-10-14',null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (7,'iPhoDAWne 4S','2011-10-14',null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (8,'iPhone 4S','2011-10-14',null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (9,'iPhone 4S','2011-10-14',null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (10,'iPhone 4S','2011-10-14',null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (11,'iPhone 4S','2011-10-14',null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (12,'iPhone 4S','2011-10-14',null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (13,'iPhone 4S','2011-10-14',null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (14,'iPhone 4S','2011-10-14',null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (15,'iPhone 4S','2011-10-14',null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (16,'iPhone 4S','2011-10-14',null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (17,'iPhone 4S','2011-10-14',null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (18,'iPhone 4S','2011-10-14',null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (19,'iPhone 4S','2011-10-14',null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (20,'iPhone 4S','2011-10-14',null,1);
 insert into computer (id,name,introduced,discontinued,company_id) values (574,'iPhone 4S','2011-10-14',null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (574,'iPhone 4S','2011-10-14',null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (574,'iPhone 4S','2011-10-14',null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (574,'iPhone 4S','2011-10-14',null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (574,'iPhone 4S','2011-10-14',null,1);
+
 
 # --- !Downs
 
